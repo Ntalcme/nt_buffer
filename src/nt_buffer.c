22c4834edac3c222a2f63981ca80658144d60aea
Ntalcme <57364571+Ntalcme@users.noreply.github.com>
2025-09-20 01:57:15 +0200

    add a clear separation

--- src/nt_buffer.c
+++ src/nt_buffer.c
@@ -1,5 +1,6 @@
 #include "nt_buffer.h"
 #include <stddef.h>
+#include <stdlib.h>
 #include <string.h>
 
 struct nt_buffer
@@ -189,34 +190,4 @@ void nt_buffer_remove(nt_buffer *buf, size_t i)
            bytes_to_move);
 
     buf->element_count--;
-}
-
-/**
- * Reallocs only the necessary memory for the buffer's data
- * @param buf The buffer's pointer
- * @return the appropriate success/failure code
- */
-nt_buffer_return_code nt_buffer_shrink_to_fit(nt_buffer *buf)
-{
-    void *new_data;
-
-    if (!buf || !buf->data || buf->element_count == buf->capacity)
-        return (NT_BUFFER_SUCCESS);
-
-    if (buf->element_count == 0)
-    {
-        free(buf->data);
-        buf->data = NULL;
-        buf->capacity = 0;
-        return (NT_BUFFER_SUCCESS);
-    }
-
-    new_data = realloc(buf->data, buf->element_count * buf->element_size);
-
-    if (!new_data)
-        return (NT_BUFFER_FAILURE_MALLOC);
-    buf->data = new_data;
-    buf->capacity = buf->element_count;
-
-    return (NT_BUFFER_SUCCESS);
 }
