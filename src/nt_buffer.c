c9700991fb2ed90334a11aa9b764223dd69e60ef
Ntalcme <57364571+Ntalcme@users.noreply.github.com>
2025-09-19 23:54:40 +0200

    clear all the elements

--- src/nt_buffer.c
+++ src/nt_buffer.c
@@ -114,3 +114,22 @@ void nt_buffer_delete(nt_buffer **buf_ptr)
     free(*buf_ptr);
     *buf_ptr = NULL;
 }
+
+/**
+ * Clear all elements of the buffer but keeps the allocated memory
+ * @param buf The buffer's pointer
+ */
+void nt_buffer_clear(nt_buffer *buf)
+{
+    if (!buf)
+        return;
+
+    if (buf->destructor && buf->data)
+    {
+        for (size_t i = 0; i < buf->element_count; i++)
+        {
+            buf->destructor((char *)buf->data + (i * buf->element_size));
+        }
+    }
+    buf->element_count = 0;
+}
